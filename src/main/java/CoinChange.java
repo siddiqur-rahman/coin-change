364ef9436cca7bb478f7611e4d4cc1f1eaa48f08
Siddiqur Rahman <siddiqur.rahman077@gmail.com>
2026-06-28 14:14:49 +0600

    update the test

--- src/main/java/CoinChange.java
+++ src/main/java/CoinChange.java
@@ -2,11 +2,20 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class CoinChange {
+    public Map<Integer, Integer> make(int amount) throws Exception {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        }
 
-    public Map<Integer, Integer> make(int amount) {
         Map<Integer, Integer> changes = new HashMap<>();
 
-        changes.put(1, 1);
+        Integer numOfCoin5 = amount/5;
+        Integer numOfCoin2 = (amount-(5*numOfCoin5))/2;
+        Integer numOfCoin1 = amount-(5*numOfCoin5) - (2*numOfCoin2);
+
+        changes.put(5, numOfCoin5);
+        changes.put(2, numOfCoin2);
+        changes.put(1, numOfCoin1);
 
         return changes;
     }
--- src/test/java/CoinChangeTest.java
+++ src/test/java/CoinChangeTest.java
@@ -4,11 +4,12 @@ import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CoinChangeTest {
 
     @Test
-    public void testChangesOf1() {
+    public void testChangesOf1() throws Exception {
         CoinChange coinChange = new CoinChange();
 
         Map<Integer, Integer> changes = coinChange.make(1);
@@ -16,4 +17,60 @@ public class CoinChangeTest {
         assertThat(changes.get(1), is(1));
     }
 
+    @Test
+    public void testChangesOf9() throws Exception {
+        CoinChange coinChange = new CoinChange();
+
+        Map<Integer, Integer> changes = coinChange.make(9);
+
+        assertThat(changes.get(5), is(1));
+        assertThat(changes.get(2), is(2));
+        assertThat(changes.get(1), is(0));
+    }
+
+    @Test
+    public void testChangesOf6() throws Exception {
+        CoinChange coinChange = new CoinChange();
+
+        Map<Integer, Integer> changes = coinChange.make(6);
+
+        System.out.println(changes);
+
+        assertThat(changes.get(5), is(1));
+        assertThat(changes.get(2), is(0));
+        assertThat(changes.get(1), is(1));
+    }
+
+    @Test
+    public void testChangesOf8() throws Exception {
+        CoinChange coinChange = new CoinChange();
+
+        Map<Integer, Integer> changes = coinChange.make(8);
+
+        System.out.println(changes);
+
+        assertThat(changes.get(5), is(1));
+        assertThat(changes.get(2), is(1));
+        assertThat(changes.get(1), is(1));
+    }
+
+    @Test
+    public void testChangesOf0() throws Exception {
+        CoinChange coinChange = new CoinChange();
+
+        Map<Integer, Integer> changes = coinChange.make(0);
+
+        System.out.println(changes);
+
+        assertThat(changes.get(5), is(0));
+        assertThat(changes.get(2), is(0));
+        assertThat(changes.get(1), is(0));
+    }
+
+    @Test
+    public void testChangesOfNeg8() {
+        CoinChange coinChange = new CoinChange();
+        assertThrows(IllegalArgumentException.class, () -> coinChange.make(-8));
+
+    }
 }
